a23e05e2f74b40f9e11ac3e7719c520bd3203979
Alexandre Pauwels <alex@pauwelslabs.com>
2023-10-05 17:39:41 +0100

    fix: adds verification of slack request

--- main.go
+++ main.go
@@ -1,15 +1,97 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 
+	viperpit "github.com/ajpauwels/pit-of-vipers"
 	"go.uber.org/zap"
 )
 
-func HandlerWithLogger(logger *zap.Logger) func(http.ResponseWriter, *http.Request) {
+type SlackConfig struct {
+	SigningKey string `mapstructure:"signingkey"`
+}
+
+type Config struct {
+	Port  uint16      `mapstructure:"port"`
+	Slack SlackConfig `mapstructure:"slack"`
+}
+
+func BuildHandler(logger *zap.Logger, config *Config) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger.Info("handling http request", zap.String("path", r.URL.Path))
+
+		// Ensure the request uses the POST method
+		method := r.Method
+		if method != "POST" {
+			logger.Error("incorrect request method", zap.String("method", method))
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			io.WriteString(w, "error: must be a POST request")
+			return
+		}
+
+		// Ensure the request includes a signature header
+		signatureHeader := []byte(r.Header.Get("x-slack-signature"))
+		if len(signatureHeader) == 0 {
+			logger.Error("missing request x-slack-signature-header")
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, "error: must include the x-slack-signature header")
+			return
+		}
+
+		// Ensure the request includes a timestamp header
+		timestampHeader := []byte(r.Header.Get("x-slack-request-timestamp"))
+		if len(timestampHeader) == 0 {
+			logger.Error("missing request x-slack-request-timestamp header")
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, "error: must include the x-slack-request-timestamp header")
+			return
+		}
+
+		// Parse the form body as a string
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Error("unable to parse request body", zap.Error(err))
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusInternalServerError)
+			io.WriteString(w, "error: unable to read request body")
+			return
+		}
+
+		// Create the secured request signature using the Slack signing key
+		baseString := fmt.Sprintf("v0:%s:%s", timestampHeader, body)
+		mac := hmac.New(sha256.New, []byte(config.Slack.SigningKey))
+		bytesWritten, err := mac.Write([]byte(baseString))
+		if err != nil {
+			logger.Error("unable to compute request signature", zap.Error(err), zap.Int("bytesWritten", bytesWritten))
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusInternalServerError)
+			io.WriteString(w, "error: unable to compute request signature")
+			return
+		}
+		signatureComputed := mac.Sum(nil)
+
+		// Compare the generated signature with the provided signature
+		if !hmac.Equal(signatureHeader, signatureComputed) {
+			logger.Error("computed signature and provided signature do not match", zap.String("computed", string(signatureComputed)), zap.String("provided", string(signatureHeader)))
+			w.Header().Set("content-type", "text/plain; charset=utf-8")
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, "error: computed signature and provided signature do not match")
+			return
+		}
+
+		w.Header().Set("content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "ok")
 	}
 }
 
@@ -21,11 +103,40 @@ func main() {
 	}
 	defer logger.Sync()
 
-	// Define handlers
-	http.HandleFunc("/", HandlerWithLogger(logger))
+	// Load configuration
+	vpCh, errCh := viperpit.NewFromPathsAndName([]string{"./config"}, "base")
+	for {
+		select {
+		case vp := <-vpCh:
+			// Workaround to add ENV prefix and be able to unmarshal env-provided values
+			vp.SetEnvPrefix("SERVICE")
+			vp.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+			for _, key := range vp.AllKeys() {
+				val := vp.Get(key)
+				vp.Set(key, val)
+			}
 
-	// Start server
-	logger.Info("starting server", zap.Int("port", 8080))
-	err = http.ListenAndServe(":8080", nil)
-	logger.Error("failed to start http server", zap.Error(err))
+			// Unmarshal config into struct
+			var config Config
+			vp.Unmarshal(&config)
+
+			logger.Info("config", zap.Uint16("port", config.Port), zap.String("slack.signingkey", string(config.Slack.SigningKey)))
+
+			// Define handlers
+			mux := http.NewServeMux()
+			mux.HandleFunc("/", BuildHandler(logger, &config))
+
+			// Start server
+			logger.Info("starting server", zap.Uint16("port", config.Port))
+			err = http.ListenAndServe(fmt.Sprintf(":%d", config.Port), mux)
+			if errors.Is(err, http.ErrServerClosed) {
+				logger.Info("server has shutdown normally")
+				break
+			} else {
+				logger.Fatal("failed to start http server", zap.Error(err))
+			}
+		case err := <-errCh:
+			logger.Error("error loading config", zap.Error(err))
+		}
+	}
 }
